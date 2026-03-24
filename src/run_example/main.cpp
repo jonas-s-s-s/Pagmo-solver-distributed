eebbb422c6113966ecc59c1f8aab6d7c3e6c9a59
Jonas <jonasx.dufek@gmail.com>
2026-03-25 00:48:37 +0100

    Benchmark single objective example

--- src/run_example/main.cpp
+++ src/run_example/main.cpp
@@ -8,7 +8,9 @@
 #include "solver_benchmark.h"
 #include "udp_dll_wrapper.h"
 #include "udp_registry.h"
+#include "pagmo/algorithms/de.hpp"
 #include "pagmo/algorithms/gaco.hpp"
+#include "pagmo/problems/rosenbrock.hpp"
 #include "pagmo/problems/zdt.hpp"
 #include "pagmo/utils/hypervolume.hpp"
 
@@ -17,10 +19,8 @@ int main(int argc, char* argv[])
 {
     AixLog::Log::init<AixLog::SinkCout>(AixLog::Severity::trace);
 
-
+    /*
     const pagmo::problem prob{pagmo::zdt{1}};
-
-
     solver_benchmark s{prob};
 
     {
@@ -64,7 +64,52 @@ int main(int argc, char* argv[])
     }
 
     std::cout << s.get_benchmark_stats_csv() << std::endl;
+    */
 
+
+    const pagmo::problem prob{pagmo::rosenbrock{10}}; // single-objective
+    solver_benchmark s{prob};
+
+    {
+        const pagmo::population pop{prob, 100};
+        const pagmo::de algo{50};
+        const auto out = algo.evolve(pop);
+        const auto best = select_best_individual(prob, out.get_x(), out.get_f());
+        s.add_result(best, "First");
+    }
+
+    {
+        const pagmo::population pop{prob, 60};
+        const pagmo::de algo{50};
+        const auto out = algo.evolve(pop);
+        const auto best = select_best_individual(prob, out.get_x(), out.get_f());
+        s.add_result(best, "Second");
+    }
+
+    {
+        const pagmo::population pop{prob, 40};
+        const pagmo::de algo{50};
+        const auto out = algo.evolve(pop);
+        const auto best = select_best_individual(prob, out.get_x(), out.get_f());
+        s.add_result(best, "Third");
+    }
+
+    {
+        const pagmo::population pop{prob, 40};
+        const pagmo::de algo{30};
+        const auto out = algo.evolve(pop);
+        const auto best = select_best_individual(prob, out.get_x(), out.get_f());
+        s.add_result(best, "Fourth");
+    }
+
+    {
+        const pagmo::population pop{prob, 40};
+        const pagmo::de algo{20};
+        const auto out = algo.evolve(pop);
+        const auto best = select_best_individual(prob, out.get_x(), out.get_f());
+        s.add_result(best, "Fifth");
+    }
+    std::cout << s.get_benchmark_stats_csv() << std::endl;
 
     return 0;
 }
