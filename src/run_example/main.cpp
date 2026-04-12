d4f06e3a5c6c1a2b9eef6e0bc099d03b9efc15b2
Jonas <jonasx.dufek@gmail.com>
2026-04-12 22:44:16 +0200

    Merge normal main and benchmark main
    Added CMD line parameter parsing into main, merging these two files removes duplicate code

--- src/run_example/main.cpp
+++ src/run_example/main.cpp
@@ -7,34 +7,58 @@
 #include "pagmo/algorithms/de.hpp"
 #include "pagmo/algorithms/gaco.hpp"
 
+#include <iostream>
+#include <string>
+#include <stdexcept>
 
-void run_controller(const std::string& address, size_t expectedWorkerCount,
-                    load_balancing_strategy loadBalancingStrategy, const std::string& localCacheDir)
+#include "benchmark_stats.h"
+#include "so_benchmark.h"
+
+// ####################################################
+// # DIFFERENT CONTROLLER TASKS
+// ####################################################
+
+void benchmark_controller_task(const std::string& address, const size_t expectedWorkerCount,
+                               const load_balancing_strategy loadBalancingStrategy, const std::string& localCacheDir)
 {
-    std::string errMsg = "Aborting distributed solver, an excpetion occured: ";
+    udp_registry::get().set_local_cache_dir(localCacheDir);
+
+    distributed_solver ds{address, expectedWorkerCount, loadBalancingStrategy};
+    benchmark_stats bench{};
+    run_so_benchmark(ds, bench);
+}
+
+void default_controller_task(const std::string& address, const size_t expectedWorkerCount,
+                             const load_balancing_strategy loadBalancingStrategy, const std::string& localCacheDir)
+{
+    udp_registry::get().set_local_cache_dir(localCacheDir);
+    unsigned param = 2;
+    udp_dll_wrapper probWrapper{"schwefel_udp", std::any{param}};
+    const pagmo::problem prob{probWrapper};
+    pagmo::algorithm algo{pagmo::de(1500)};
+    algo.set_verbosity(0);
+    distributed_solver ds{address, expectedWorkerCount, loadBalancingStrategy};
+    ds.wait_until_workers_connect();
+    ds.evolve(prob, {algo}, 1000);
+    ds.wait_until_completion();
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    ds.evolve(prob, {algo}, 1000);
+    ds.wait_until_completion();
+    std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+}
+
+// ####################################################
+// # CONTROLLER / WORKER RUN
+// ####################################################
+
+void run_controller(const std::string& address, const size_t expectedWorkerCount,
+                    const load_balancing_strategy loadBalancingStrategy, const std::string& localCacheDir,
+                    const std::function<void(std::string, size_t, load_balancing_strategy, std::string)>& runFunc)
+{
+    std::string errMsg = "Aborting distributed solver, an exception occurred: ";
     try
     {
-        udp_registry::get().set_local_cache_dir(localCacheDir);
-
-        unsigned param = 2;
-        udp_dll_wrapper probWrapper{"schwefel_udp", std::any{param}};
-        const pagmo::problem prob{probWrapper};
-
-        pagmo::algorithm algo{pagmo::de(1500)};
-        algo.set_verbosity(0);
-
-        distributed_solver ds{address, expectedWorkerCount, loadBalancingStrategy};
-        ds.wait_until_workers_connect();
-
-        ds.evolve(prob, {algo}, 1000);
-        ds.wait_until_completion();
-
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-
-        ds.evolve(prob, {algo}, 1000);
-        ds.wait_until_completion();
-
-        std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+        runFunc(address, expectedWorkerCount, loadBalancingStrategy, localCacheDir);
     }
     catch (const std::exception& e)
     {
@@ -62,64 +86,226 @@ void run_controller(const std::string& address, size_t expectedWorkerCount,
     }
 }
 
-void run_worker(const std::string& address, worker_mode mode, const size_t minIslandPopSize, const std::string& localCacheDir)
+void run_worker(const std::string& address, worker_mode mode,
+                size_t minIslandPopSize, const std::string& localCacheDir)
 {
-    std::string errMsg = "Aborting distributed worker, an excpetion occured: ";
+    std::string errMsg = "Aborting distributed worker, an exception occurred: ";
     try
     {
-
         distributed_worker worker{address, mode, minIslandPopSize};
-        // We register this worker with the UDP registry, so DLLs can be requested from controller
         udp_registry::get().set_local_cache_dir(localCacheDir);
         udp_registry::get().register_udp_provider(
-            [&worker](const std::string& libName)
-            {
-                return worker.get_dll_from_controller(libName);
-            }
-        );
+            [&worker](const std::string& libName) { return worker.get_dll_from_controller(libName); });
+        for (;;) worker.client_loop();
+    }
+    catch (const std::exception& e)
+    {
+        errMsg += e.what();
+        LOG(FATAL) << errMsg;
+        throw std::runtime_error(errMsg);
+    }
+    catch (const std::string& e)
+    {
+        errMsg += e;
+        LOG(FATAL) << errMsg;
+        throw std::runtime_error(errMsg);
+    }
+    catch (const char* e)
+    {
+        errMsg += e;
+        LOG(FATAL) << errMsg;
+        throw std::runtime_error(errMsg);
+    }
+    catch (...)
+    {
+        errMsg += "unknown exception type";
+        LOG(FATAL) << errMsg;
+        throw std::runtime_error(errMsg);
+    }
+}
 
-        for (;;)
+// ####################################################
+// # PARSING
+// ####################################################
+
+void print_help(char** argv)
+{
+    std::cout <<
+        "Usage:\n"
+        "  " << argv[0] << " --controller [options]\n"
+        "  " << argv[0] << " --worker     [options]\n"
+        "\n"
+        "Controller options:\n"
+        "  --address      <addr>  (default: tcp://0.0.0.0:5000)\n"
+        "  --workers      <n>     Expected worker count (default: 2)\n"
+        "  --strategy     <name>  (default: BY_PERFORMANCE)\n"
+        "  --cache-dir    <dir>   (default: controller_cache)\n"
+        "\n"
+        "Worker options:\n"
+        "  --address      <addr>  (default: tcp://localhost:5000)\n"
+        "  --mode         <name>  (default: SINGLE_THREADED)\n"
+        "  --min-pop-size <n>     (default: 80)\n"
+        "  --cache-dir    <dir>   (default: worker_cache)"
+        << std::endl;
+}
+
+struct main_args
+{
+    bool isController = false;
+
+    std::string address = ""; // Set by user
+    std::string defaultAddressWorker = "tcp://localhost:5000";
+    std::string defaultAddressController = "tcp://localhost:5000";
+    size_t workers = 2;
+
+    std::string strategy = "BY_PERFORMANCE";
+    std::string workerMode = "SINGLE_THREADED";
+    size_t minPopSize = 80;
+
+    std::string cacheDir = ""; // Set by user
+    std::string defaultCacheDirWorker = "worker_cache";
+    std::string defaultCacheDirController = "controller_cache";
+
+    bool runBenchmark = false;
+    bool disableLogging = false;
+};
+
+std::optional<main_args> parse_main_args(const int argc, char** argv)
+{
+    main_args mainArgs{};
+    bool modeSet = false;
+
+    for (int i = 1; i < argc; ++i)
+    {
+        std::string a = argv[i];
+        auto nextArg = [&]() -> std::string
         {
-            worker.client_loop();
+            if (++i >= argc)
+                throw std::runtime_error(a + " requires a value");
+            return argv[i];
+        };
+
+        if (a == "--help" || a == "-h")
+        {
+            // handled in main
+        }
+        else if (a == "--controller")
+        {
+            mainArgs.isController = true;
+            modeSet = true;
+        }
+        else if (a == "--benchmark")
+        {
+            mainArgs.runBenchmark = true;
+        }
+        else if (a == "--disable-logging")
+        {
+            mainArgs.disableLogging = true;
+        }
+        else if (a == "--worker")
+        {
+            mainArgs.isController = false;
+            modeSet = true;
+        }
+        else if (a == "--address")
+        {
+            mainArgs.address = nextArg();
+        }
+        else if (a == "--workers")
+        {
+            mainArgs.workers = std::stoul(nextArg());
+        }
+        else if (a == "--strategy")
+        {
+            mainArgs.strategy = nextArg();
+        }
+        else if (a == "--mode")
+        {
+            mainArgs.workerMode = nextArg();
+        }
+        else if (a == "--min-pop-size")
+        {
+            mainArgs.minPopSize = std::stoul(nextArg());
+        }
+        else if (a == "--cache-dir")
+        {
+            mainArgs.cacheDir = nextArg();
+        }
+        else
+        {
+            std::cerr << "Unknown argument: " << a << "\n";
+            modeSet = false;
+            break;
+        }
+    }
+
+    if (!modeSet)
+    {
+        return std::nullopt;
+    }
+    return mainArgs;
+}
+
+// ####################################################
+// # MAIN
+// ####################################################
+int main(int argc, char* argv[])
+{
+    // Parse cmd line args
+    const auto& mainArgsOpt = parse_main_args(argc, argv);
+    if (!mainArgsOpt.has_value())
+    {
+        print_help(argv);
+        return 2;
+    }
+    const auto& ma = mainArgsOpt.value();
+
+    if (ma.disableLogging)
+    {
+        AixLog::Log::init<AixLog::SinkCout>(AixLog::Severity::fatal);
+    }
+    else
+    {
+        AixLog::Log::init<AixLog::SinkCout>(AixLog::Severity::trace);
+    }
+
+    // Run controller / worker
+    try
+    {
+        if (ma.isController)
+        {
+            const auto controllerTask = (ma.runBenchmark) ? benchmark_controller_task : default_controller_task;
+
+            const load_balancing_strategy lbs = (ma.strategy == "ALL_ISLANDS_EQUAL")
+                                                    ? load_balancing_strategy::ALL_ISLANDS_EQUAL
+                                                    : load_balancing_strategy::BY_PERFORMANCE;
+
+            run_controller(
+                ma.address.empty() ? ma.defaultAddressController : ma.address,
+                ma.workers,
+                lbs,
+                ma.cacheDir.empty() ? ma.defaultCacheDirController : ma.cacheDir,
+                controllerTask
+            );
+        }
+        else
+        {
+            const worker_mode wm = (ma.workerMode == "ARCHIPELAGO_BASED")
+                                       ? worker_mode::ARCHIPELAGO_BASED
+                                       : worker_mode::SINGLE_THREADED;
+
+            run_worker(
+                ma.address.empty() ? ma.defaultAddressWorker : ma.address,
+                wm,
+                ma.minPopSize,
+                ma.cacheDir.empty() ? ma.defaultCacheDirWorker : ma.cacheDir
+            );
         }
     }
     catch (const std::exception& e)
     {
-        errMsg += e.what();
-        LOG(FATAL) << errMsg;
-        throw std::runtime_error(errMsg);
+        return 2;
     }
-    catch (const std::string& e)
-    {
-        errMsg += e;
-        LOG(FATAL) << errMsg;
-        throw std::runtime_error(errMsg);
-    }
-    catch (const char* e)
-    {
-        errMsg += e;
-        LOG(FATAL) << errMsg;
-        throw std::runtime_error(errMsg);
-    }
-    catch (...)
-    {
-        errMsg += "unknown exception type";
-        LOG(FATAL) << errMsg;
-        throw std::runtime_error(errMsg);
-    }
-}
 
-int main(int argc, char* argv[])
-{
-    AixLog::Log::init<AixLog::SinkCout>(AixLog::Severity::trace);
-
-    if (argc >= 2 && argv[1] == std::string("-run-controller"))
-    {
-        run_controller("tcp://0.0.0.0:5000", 2, load_balancing_strategy::BY_PERFORMANCE, "controller_cache");
-    }
-    else
-    {
-        run_worker("tcp://localhost:5000", worker_mode::SINGLE_THREADED, 80, "worker_cache");
-    }
     return 0;
 }
